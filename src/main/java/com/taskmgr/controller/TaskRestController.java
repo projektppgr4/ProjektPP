a2f038b145c4af41823991786916319d26dd42d7
Akai <romek1248@gmail.com>
2017-05-11 16:41:36 +0200

    Wersja do pokazania

--- src/main/java/com/taskmgr/controller/TaskRestController.java
+++ src/main/java/com/taskmgr/controller/TaskRestController.java
@@ -1,11 +1,6 @@
 package com.taskmgr.controller;
 
-import com.taskmgr.dao.TaskDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by Akai on 2017-03-29.
@@ -14,12 +9,5 @@ import java.util.List;
 @RestController
 public class TaskRestController {
 
-	@Autowired
-	private TaskDao taskDao;
-
-	@GetMapping("/Task")
-	public List getTask() {
-		return taskDao.findAll();
-	}
 
 }
